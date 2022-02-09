718fc2ab26fc800a88ef9cf27dfa974923c3665c
paolo <paoloc9997@gmail.com>
2022-02-09 20:55:43 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import {Routes,Route} from 'react-router-dom';
+import {Navigate} from 'react-router-dom';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar';
+import ProductList from './components/ProductList';
+import Details from './components/Details';
+import Cart from './components/Cart/Cart';
+import Default from './components/Default';
+import Modal from './components/Modal';
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<ProductList/>} />
+          <Route path="/details" element={<Details/>} />
+          <Route path="/cart" element={<Cart/>} />
+          <Route path={"/default"} element={<Default/>} />
+          <Route path="*" element={<Navigate to="/default" />}/>
+        </Routes>
+
+        <Modal />
+      </React.Fragment>
+
+    );
+
+  }
+}
+
+
+
+
+export default App;
